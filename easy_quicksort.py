7ec025460bff18fbe71e765788d2d4e58288c397
임준엽 <imjun-yeob@imjun-yeobs-MacBook-Air.local>
2022-10-10 14:17:41 +0900

    quick sort programming assignment add

--- easy_quicksort.py
+++ easy_quicksort.py
@@ -1,45 +1,30 @@
 import random
-import numpy as np
 
-def pivot(input_arr : list, used_item : list):
-    N = len(input_arr)
-    P = int(N/2) 
-    while (P in used_item):
-        P = random.choice(input_arr)
-    used_item.append(P)
-    left_arr = []
-    right_arr = []
-    for idx, num in enumerate(input_arr):
-        if (num > P):
-            right_arr.append(idx)
-        elif (num == P):
-            pass
+
+def quick_sort(arr:list, i:int):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            lesser_arr.append(num)
+        elif num > pivot:
+            greater_arr.append(num)
         else:
-            left_arr.append(idx)
-    output_arr = [0 for i in left_arr]
-    for idx, num in enumerate(left_arr):
-        output_arr[idx] = input_arr[num]
-    output_arr.append(P)
-    for num in right_arr:
-        output_arr.append(input_arr[num])
-    return output_arr, used_item
-
-def quick_sort(input_arr : list):    
-    correct_arr = [i for i in range(len(input_arr))]
-    pivot_time = 0
-    used_item = []
-    while True:
-        input_arr, used_item = pivot(input_arr, used_item)
-        pivot_time += 1
-        if (input_arr == correct_arr):
-            break
-    return input_arr, pivot_time
+            equal_arr.append(num)
+        i += 1
+    print(i)
+    return quick_sort(lesser_arr, i) + equal_arr + quick_sort(greater_arr, i)
+    
 
 if (__name__ == "__main__"):
-    input_arr = [i for i in range(30)]
-    random.shuffle(input_arr)
-    print("first arr : ", input_arr)
-    output_arr, pivot_time = quick_sort(input_arr)
-    print("output : ", output_arr)
-    print("pivot used : ", pivot_time)
-    
+    input_arr = []
+    with open("./input_arr.txt") as f:
+        lines = f.readlines()
+        for line in lines:
+            input_arr.append(int(line))
+            
+    i = 0
+    output_arr = quick_sort(input_arr, i)
+    print("number of compare : ", i)
